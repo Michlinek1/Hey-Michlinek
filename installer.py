4699015566510efddfb60548e1bd44c1690c76f4
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-02-23 22:59:55 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,4 +1,6 @@
 import os
+import time
+import sys
 
 def Installing():
     with open("requirements.txt", "r") as f:
@@ -13,8 +15,12 @@ def Installing():
         from selenium import webdriver
         from webdriver_manager.chrome import ChromeDriverManager
         from translate import Translator
+        from langdetect import detect, DetectorFactory
     except ImportError:
         os.system("pip install -r requirements.txt")
+        print("Program reset is required!")
+        time.sleep(1)
+        sys.exit()
 
 
     
